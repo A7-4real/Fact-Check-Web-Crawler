3b237e28c8d719f38477c93fe2248b50238eaf5e
A7-4real <a7.4real@gmail.com>
2022-04-17 23:35:44 +0530

    define the fields for FactcheckWebCrawlerItem

--- Fact_Check_Web_Crawler/items.py
+++ Fact_Check_Web_Crawler/items.py
@@ -3,10 +3,41 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
+from itemadapter import ItemAdapter
+
+# metadata class
 
 
-class FactCheckWebCrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class Metadata(Item):
+
+    # image context metadata
+    news_src = Field(serializer=str)
+    src_article_url = Field(serializer=str)
+    # image url collected from different css selectors stored in python dict
+    images_url = Field(serializer=dict)
+    article_heading = Field(serializer=str)
+    short_context = Field(serializer=str)
+    short_intro = Field(serializer=str)
+    author = Field(serializer=str)
+    date = Field(serializer=str)
+
+
+class Image_context(Item):
+
+    # image context as a whole
+    id = Field(serializer=str)
+    image_url = Field(serializer=str)
+    metadata = Field(serializer=Metadata)         # Metadata item
+
+
+class FactCheckWebCrawlerItem(Item):
+
+    # apended Images_context items
+    image_context = Field(serializer=Image_context)
     pass
+
+
+# adapterMetadata = ItemAdapter(Metadata)
+# adapterImageContext = ItemAdapter(Image_context)
+# adapterFcw = ItemAdapter(FactCheckWebCrawlerItem)
